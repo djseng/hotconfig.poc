941db7b72a7fba771eba89deb100f6df2145d38a
David Seng <david@polaris.llc>
2022-01-25 16:37:21 -0600

    poc

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+//configure.AddAzureKeyVault("https://hotconfig-poc-kv.vault.azure.net/");
+var hostBuilder = new HostBuilder();
+hostBuilder
+    .ConfigureHostConfiguration(
+        configure => configure.AddJsonFile("appconfig.json", false))
+    .ConfigureServices(
+        (ctx, services) =>
+        {
+            services.AddLogging(lb => lb.AddConfiguration(ctx.Configuration));
+            services.AddHttpClient();
+            services.AddHostedService<ProofOfConceptService>();
+        });
+var host = hostBuilder.Build();
+try
+{
+    await host.RunAsync();
+}
+finally
+{
+    host.Dispose();
+}
---
+++ ProofOfConceptService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+class ProofOfConceptService : BackgroundService 
+{ 
+    private HttpClient _http;
+    private ILogger<ProofOfConceptService> _logger;
+
+    public ProofOfConceptService(HttpClient http, ILogger<ProofOfConceptService> logger)
+    {
+        _http = http;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            Console.WriteLine("-- burp --");
+            _logger.LogInformation("GET");
+            await _http.GetAsync("https://google.com");
+            _logger.LogInformation("DONE");
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+        }
+    }
+}
+
